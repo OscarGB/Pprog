1bad95a1c8fefb19ad91be1f98bc9fa6db010bbe
Óscar Gómez <oscar.gomez.borz@gmail.com>
2016-10-07 17:07:29 +0200

    dado a falta de comprobaciones

--- codigo/cabeceras/die.h
+++ codigo/cabeceras/die.h
@@ -12,11 +12,16 @@ Description: It defines the die's commands
 Revision history: none
 
 =================================================================== */
+#ifndef DIE_H
+#define DIE_H
 
 #include <stdio.h>
+#include <types.h>
+
+typedef struct _Die Die;
 
 /* --------------------------------------------------------------------
-Function:die_throw
+Function:die_roll()
 
 Date: 07/10/2016
 
@@ -26,7 +31,92 @@ Description: It return a random value between inf and sup
 
 Input: int inf (The inferior limit)
 	   int sup (The superior limit)
+	   Die* (The die to roll)
 Output: int (the random number)
 
 ------------------------------------------------------------------- */
-int die_throw(int inf, int sup)
+int die_roll(Die* die, int inf, int sup);
+
+
+/* --------------------------------------------------------------------
+Function: die_create();
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It creates a die without any information inside
+
+Input: Id (The id of the dice)
+
+Output: Die* (A pointer to the created die)
+
+------------------------------------------------------------------- */
+Die* die_create(Id id);
+
+
+/* --------------------------------------------------------------------
+Function: die_destroy()
+
+Date:07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It destroy a dice, freeing all the memory blocks
+
+Input: Die* (The die to destroy)
+
+Output: STATUS (OK if everything went well, ERROR if didn't)
+
+------------------------------------------------------------------- */
+STATUS die_destroy(Die* die);
+
+/* --------------------------------------------------------------------
+Function: die_print()
+
+Date: 04/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It prints the data of a die
+
+Input: Die* (The die to print)
+
+Output: STATUS (OK if everything went well, ERROR if didn't)
+
+------------------------------------------------------------------- */
+STATUS die_print(Die* die);
+
+/* --------------------------------------------------------------------
+Function: die_get_last_roll
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It returns the value of the last roll
+
+Input: Die* (The die we want to get the value)
+
+Output: int (The value of the last roll)
+
+------------------------------------------------------------------- */
+int die_get_last_roll(Die* die);
+
+/* --------------------------------------------------------------------
+Function: die_get_id()
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It returns the Id of the Die
+
+Input: Die* (The die we want to get the Id)
+
+Output: Id (The id of the die)
+
+------------------------------------------------------------------- */
+Id die_get_id(Die* die);
+
+#endif
--- codigo/die/die.c
+++ codigo/die/die.c
@@ -17,8 +17,13 @@ Revision history: none
 #include <stdio.h>
 #include "die.h"
 
+struct _Die{
+	Id id;
+	int last;
+};
+
 /* --------------------------------------------------------------------
-Function:die_throw
+Function:die_roll()
 
 Date: 07/10/2016
 
@@ -31,11 +36,10 @@ Input: int inf (The inferior limit)
 Output: int (the random number)
 
 ------------------------------------------------------------------- */
-int die_throw(int inf, int sup){
+int die_roll(Die* die, int inf, int sup){
 	float random;
 
-	if(inf < 0 || sup < 0 || sup < inf){
-		fprintf(stderr, "Error en la entrada de argumentos a aleat_num");
+	if(inf < 0 || sup < 0 || sup < inf || !die){
 		return -1; /*ERROR*/
 	}
 
@@ -46,5 +50,128 @@ int die_throw(int inf, int sup){
 	if(random > sup){
 		random = sup;
 	}
-	return (int)random;
+	die->last = (int)random;
+	return die->last;
+}
+
+/* --------------------------------------------------------------------
+Function: die_create();
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It creates a die without any information inside
+
+Input: Id (The id of the dice)
+
+Output: Die* (A pointer to the created die)
+
+------------------------------------------------------------------- */
+Die* die_create(Id id){
+	Die *die = NULL;
+
+	die = (Die *)malloc(sizeof(Die));
+
+	if(!die){
+		return NULL;
+	}
+
+	die->id = id;
+	die->last = -1;
+
+	return die;
+}
+
+
+/* --------------------------------------------------------------------
+Function: die_destroy()
+
+Date:07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It destroy a dice, freeing all the memory blocks
+
+Input: Die* (The die to destroy)
+
+Output: STATUS (OK if everything went well, ERROR if didn't)
+
+------------------------------------------------------------------- */
+STATUS die_destroy(Die* die){
+	if(!die){
+		return ERROR;
+	}
+
+	free(die);
+	die = NULL;
+
+	return OK;
+}
+
+/* --------------------------------------------------------------------
+Function: die_print()
+
+Date: 04/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It prints the data of a die
+
+Input: Die* (The die to print)
+
+Output: STATUS (OK if everything went well, ERROR if didn't)
+
+------------------------------------------------------------------- */
+STATUS die_print(Die* die){
+	if(!die){
+		return ERROR;
+	}
+
+	fprintf(stdout, "-->Die (Id: %d)", die_get_id(die));
+	fprintf(stdout, "--->Last value: %d", die_get_last_roll(die));
+
+	return OK;
+}
+
+/* --------------------------------------------------------------------
+Function: die_get_last_roll
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It returns the value of the last roll
+
+Input: Die* (The die we want to get the value)
+
+Output: int (The value of the last roll)
+
+------------------------------------------------------------------- */
+int die_get_last_roll(Die* die){
+	if(!die){
+		return -1;
+	}
+	return die->last;
+}
+
+/* --------------------------------------------------------------------
+Function: die_get_id()
+
+Date: 07/10/2016
+
+Author:Óscar Gómez, Jose Ignacio Gómez.
+
+Description: It returns the Id of the Die
+
+Input: Die* (The die we want to get the Id)
+
+Output: Id (The id of the die)
+
+------------------------------------------------------------------- */
+Id die_get_id(Die* die){
+	if(!die){
+		return NO_ID;
+	}
+	return die->id;
 }
