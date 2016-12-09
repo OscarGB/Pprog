0842ec572866876cedcd4ddc4f57951a96bb1762
Óscar Gómez <oscar.gomez.borz@gmail.com>
2016-12-09 18:06:07 +0100

    Cosas, pero no termina de funcionar

--- codigo/graphics/graphics.h
+++ codigo/graphics/graphics.h
@@ -10,11 +10,13 @@
 #define GRAPHICS_H
 
 #include "types.h"
-
+/*PLAYGROUND*/
 #define WIN1_X 42 /*!< X size of window 1*/
 #define WIN1_Y 21 /*!< Y size of window 1*/
-#define WIN2_X 37 /*!< X size of window 2*/
+/*DIALOGUES*/
+#define WIN2_X 38 /*!< X size of window 2*/
 #define WIN2_Y 21 /*!< Y size of window 2*/
+/*COMMANDS*/
 #define WIN3_X 80 /*!< X size of window 3*/
 #define WIN3_Y 3  /*!< Y size of window 3*/
 #define SPACE_SIZE_X 14 
@@ -80,11 +82,11 @@ STATUS graphics_clear_zone(Graphics* gra, ZONE zone);
 * @date 02/12/2016
 * @param Graphics* gra (The grahpcis)
 * @param ZONE zone (The zone in which you are going to print)
-* @param DIRECTION dir (The direction of the space int he playground, will be omitted if the zone is different of PLAYGROUND)
-* @param char* print (The string to be printed)
+* @param DIRECTION dir (The direction of the space in the playground and dialogue, will be omitted if the zone is different of PLAYGROUND or DIALOGUE)
+* @param char* string (The string to be printed)
 * @return STATUS (OK if everything worked, ERROR if didnt)
 */
-STATUS print_in_zone(Graphics* gra, ZONE zone, DIRECTION dir, char* print);
+STATUS print_in_zone(Graphics* gra, ZONE zone, DIRECTION dir, char* string);
 
 /**
 * @brief Refreshes the graphics
--- codigo/test/graphics_test.c
+++ codigo/test/graphics_test.c
@@ -12,8 +12,8 @@
 #include <unistd.h>
 
 
-#define MAX_TESTS 5
-#define DELAY 30
+#define MAX_TESTS 28
+#define DELAY 10
 
 BOOL test_graphics1(){
 	Graphics* gra = NULL;
@@ -72,6 +72,352 @@ BOOL test_graphics5(){
 	return TRUE;
 }
 
+BOOL test_graphics6(){
+	if(graphics_clear(NULL) != ERROR){
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics7(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, DIALOGUE, N, "HOLA");
+	print_in_zone(gra, COMMANDS, 0, "CARACOLA");
+	print_in_zone(gra, PLAYGROUND, NW, "¡JUGUEMOS!PENEPENEPENEPENEPENEPENEPENE");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear(gra) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics8(){
+	if(graphics_clear(NULL) != ERROR){
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics9(){
+	if(graphics_clear_zone(NULL, COMMANDS) != ERROR){
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics10(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	if(graphics_clear_zone(gra, 8) != ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics11(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, DIALOGUE, N,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, DIALOGUE) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics12(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, COMMANDS, 0,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, COMMANDS) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics13(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, NW,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra,PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics14(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, N,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics15(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, NE,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics16(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, W,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics17(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, C,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics18(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, E,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics19(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, SW,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics20(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, S,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics21(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, PLAYGROUND, SE,"TESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, PLAYGROUND) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics22(){
+	if(print_in_zone(NULL, PLAYGROUND, NW, "TESTING") != ERROR){
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics23(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	if(print_in_zone(gra, 8, NW, "TESTING") != ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics24(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	if(print_in_zone(gra, PLAYGROUND, 805, "TESTING") != ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics25(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	if(print_in_zone(gra, DIALOGUE, E, NULL) != ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics26(){
+	if(graphics_refresh(NULL) != ERROR){
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics27(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	print_in_zone(gra, DIALOGUE, S,"TESTINGTESTINGTESTINGTESTINGTESTINGTESTING");
+	graphics_refresh(gra);
+	sleep(DELAY);
+	if(graphics_clear_zone(gra, DIALOGUE) == ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_refresh(gra);
+	sleep(DELAY);
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
+BOOL test_graphics28(){
+	Graphics* gra = NULL;
+	gra = graphics_create();
+	if(print_in_zone(gra, DIALOGUE, 805, "TESTING") != ERROR){
+		graphics_destroy(gra);
+		TEST_PRINT(FALSE);
+		return FALSE;
+	}
+	graphics_destroy(gra);
+	TEST_PRINT(TRUE);
+	return TRUE;
+}
+
 int main(int argc, char* argv[]){
 	int test = 0;
 	int todas = 1;
@@ -93,7 +439,29 @@ int main(int argc, char* argv[]){
 	if(todas || test == 3) test_graphics3();
 	if(todas || test == 4) test_graphics4();
 	if(todas || test == 5) test_graphics5();
-
+	if(todas || test == 6) test_graphics6();
+	if(todas || test == 7) test_graphics7();
+	if(todas || test == 8) test_graphics8();
+	if(todas || test == 9) test_graphics9();
+	if(todas || test == 10) test_graphics10();
+	if(todas || test == 11) test_graphics11();
+	if(todas || test == 12) test_graphics12();
+	if(todas || test == 13) test_graphics13();
+	if(todas || test == 14) test_graphics14();
+	if(todas || test == 15) test_graphics15();
+	if(todas || test == 16) test_graphics16();
+	if(todas || test == 17) test_graphics17();
+	if(todas || test == 18) test_graphics18();
+	if(todas || test == 19) test_graphics19();
+	if(todas || test == 20) test_graphics20();
+	if(todas || test == 21) test_graphics21();
+	if(todas || test == 22) test_graphics22();
+	if(todas || test == 23) test_graphics23();
+	if(todas || test == 24) test_graphics24();
+	if(todas || test == 25) test_graphics25();
+	if(todas || test == 26) test_graphics26();
+	if(todas || test == 27) test_graphics27();
+	if(todas || test == 28) test_graphics28();
 	PRINT_RESULTS();
 
 	return 0;
