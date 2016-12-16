7a958f84ea99658401ef0e29f10280c504446684
nagomez97 <nagomez97@gmail.com>
2016-12-16 22:30:38 +0100

    dialogue

--- codigo/dialogue/dialogue.c
+++ codigo/dialogue/dialogue.c
@@ -34,7 +34,35 @@ STATUS dialogue_standard(Dialogue* dialogue, char** objects){
 
 }
 
-STATUS dialogue_go(Dialogue* dialogue, STATUS check, char** objects, Space* space){
+STATUS dialogue_generic(Dialogue* dialogue, STATUS check, char** objects, Space* space){
+	T_command cmd;
+
+	cmd = command_get_cmd(dialogue->current);
+	switch cmd{
+		case NO_CMD:
+			return dialogue_error();
+		case UNKNOWN:
+			return dialogue_error();
+		case QUIT:
+			/***/
+		case PICK:
+			return dialogue_pick();
+		case DROP:
+			return dialogue_drop();
+		case ROLL:
+			return dialogue_roll();
+		case INSPECT:
+			return dialogue_inspect();
+		case GO:
+			return dialogue_go();
+		case TURNON:
+			return dialogue_turnon();
+		case TURNOFF:
+			return dialogue_turno():
+		case OPEN:
+		case SAVE:
+		case LOAD:
+	}
 
 	if(!dialogue) return ERROR;
 
