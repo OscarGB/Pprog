99aa11fa32d67edd55db0c8f6080302dd8f49006
OscarGB <oscar.gomez.borz@gmail.com>
2016-12-21 16:17:45 +0100

    cosas

--- codigo/game/game.c
+++ codigo/game/game.c
@@ -1377,6 +1377,11 @@ STATUS callback_PICK(Game* game, Command* cmd, Dialogue* dia, Graphics* gra, cha
       if(object_get_symbol(game->object[i]) == symbol[0]){
         object = game->object[i];
 
+        if(object_get_movable(object) == FALSE){
+          objects = game_get_objects_name(game, objects);
+          dialogue_generic(dia, ERROR, objects, gra);
+          return ERROR;
+        }
         if(player_pick_object(game->player, object_get_id(object)) != FALSE){
           object_set_location(object, PLAYER_OBJ);
           objects = game_get_objects_name(game, objects);
